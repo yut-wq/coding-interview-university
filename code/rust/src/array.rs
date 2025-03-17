971e5d2d3bd96d8ac53e76fffb2715bdb45e36da
yut-wq <println856@gmail.com>
2025-03-17 19:43:20 +0900

    キャパシティの追加

--- code/rust/src/array.rs
+++ code/rust/src/array.rs
@@ -1,8 +1,30 @@
+//! 動的可変配列の実装、テスト。
+//! rustのvectorを利用する。
+//! vectorのドキュメント
+//! <https://doc.rust-lang.org/std/vec/struct.Vec.html>
+
 #[cfg(test)]
 mod tests {
+
     #[test]
     fn get_size_success() {
         let vec = vec![1, 2, 4, 5];
         assert_eq!(vec.len(), 4);
+    }
+
+    #[test]
+    fn get_capacity_success() {
+        // キャパシティーを指定してvectorを作成
+        let mut vec = Vec::with_capacity(3);
+        assert_eq!(vec.capacity(), 3);
+
+        vec.push(1);
+        vec.push(2);
+        vec.push(3);
+
+        // キャパシティーを最小単位として、vectorが拡張される
+        // 詳しくは<https://doc.rust-lang.org/std/vec/struct.Vec.html#capacity-and-reallocation>
+        vec.push(4);
+        assert_eq!(vec.capacity(), 6);
     }
 }
