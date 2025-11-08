57cbd960b02cdedcf7298671f0129a8be9d1ddf5
yut-wq <println856@gmail.com>
2025-11-08 15:21:58 +0900

    `dequeue`の実装

--- code/rust/src/queue_array.rs
+++ code/rust/src/queue_array.rs
@@ -26,11 +26,17 @@ impl QueueFromArray {
         self.tail_pointer += 1;
     }
 
-    // /// 先頭の要素を削除し、取得する
-    // #[allow(dead_code)]
-    // fn dequeue(&mut self) -> Option<i32> {
-    //     self.inner.first()
-    // }
+    /// 先頭の要素を削除し、取得する
+    #[allow(dead_code)]
+    fn dequeue(&mut self) -> Option<i32> {
+        let result = self.inner[self.head_pointer];
+
+        // 値の削除、head_pointerの更新
+        self.inner[self.head_pointer] = None;
+        self.head_pointer += 1;
+
+        result
+    }
 }
 
 #[cfg(test)]
@@ -51,17 +57,58 @@ mod tests {
         assert_eq!(queue.tail_pointer, 1);
     }
 
-    // #[test]
-    // fn dequeueで先頭の要素を削除し取得する() {
-    //     let vec = vec![1, 2, 3];
-    //     let mut queue = QueueFromArray { inner: vec };
-    //
-    //     let result = queue.dequeue();
-    //     assert_eq!(result, Some(1));
-    //
-    //     let mut inner = queue.inner.iter();
-    //     assert_eq!(inner.next(), Some(&2));
-    //     assert_eq!(inner.next(), Some(&3));
-    //     assert_eq!(inner.next(), None);
-    // }
+    #[test]
+    fn dequeueで先頭の要素を削除し取得する() {
+        let array = [
+            Some(1),
+            Some(2),
+            Some(3),
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+        ];
+        let mut queue = QueueFromArray {
+            inner: array,
+            head_pointer: 0,
+            tail_pointer: 3,
+        };
+
+        let result = queue.dequeue();
+        assert_eq!(result, Some(1));
+
+        let inner = queue.inner;
+        assert_eq!(inner[0], None);
+        assert_eq!(inner[1], Some(2));
+        assert_eq!(inner[2], Some(3));
+        assert_eq!(inner[3], None);
+
+        assert_eq!(queue.head_pointer, 1);
+        assert_eq!(queue.tail_pointer, 3);
+    }
 }
