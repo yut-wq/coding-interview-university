ec6b2afa2bbd2f82f346f316f8530f9fb20cc705
yut-wq <println856@gmail.com>
2025-11-08 14:05:59 +0900

    キューから要素を削除する関数の実装

--- code/rust/src/queue.rs
+++ code/rust/src/queue.rs
@@ -22,6 +22,15 @@ impl QueueFromLinkedList {
     fn enqueue(&mut self, element: i32) {
         self.inner.push_back(element);
     }
+
+    /// 先頭の要素を削除し、取得する
+    #[allow(dead_code)]
+    fn dequeue(&mut self) -> i32 {
+        match self.inner.pop_front() {
+            Some(elm) => elm,
+            None => todo!(),
+        }
+    }
 }
 
 // enqueueができること
@@ -39,4 +48,20 @@ mod tests {
         assert_eq!(inner.next(), Some(&1));
         assert_eq!(inner.next(), None);
     }
+
+    #[test]
+    fn dequeueで先頭の要素を削除し取得する() {
+        let list = LinkedList::from([1, 2, 3]);
+        let mut queue = QueueFromLinkedList { inner: list };
+
+        let result = queue.dequeue();
+        assert_eq!(result, 1);
+
+        let mut inner = queue.inner.iter();
+        assert_eq!(inner.next(), Some(&2));
+        assert_eq!(inner.next(), Some(&3));
+        assert_eq!(inner.next(), None);
+    }
+
+    // dequeueで要素がないとき
 }
